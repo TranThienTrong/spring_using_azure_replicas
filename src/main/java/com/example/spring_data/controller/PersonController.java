38bf8048484d5027d23f67e5a2bb8ca5e8ff389a
Thien Trong <tranthientrong.it@gmail.com>
2022-10-03 12:37:28 +0700

    Add Service

--- src/main/java/com/example/spring_data/controller/PersonController.java
+++ src/main/java/com/example/spring_data/controller/PersonController.java
@@ -4,6 +4,8 @@ import com.example.spring_data.model.Person;
 import com.example.spring_data.repository.primary.PrimaryPersonRepository;
 
 import com.example.spring_data.repository.secondary.SecondaryPersonRepository;
+import com.example.spring_data.service.PersonService;
+import com.example.spring_data.service.ReadOnlyPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,85 +20,40 @@ import java.util.Optional;
 @RequestMapping("/")
 public class PersonController {
     @Autowired
-    PrimaryPersonRepository primaryPersonRepository;
+    PersonService personService;
 
     @Autowired
-    SecondaryPersonRepository secondaryPersonRepository;
+    ReadOnlyPersonService readOnlyPersonService;
 
 
     @GetMapping("/persons")
-    public ResponseEntity<List<Person>> getAllPersons(@RequestParam(required = false) String name) {
-        try {
-            List<Person> personList = new ArrayList<>();
-
-            personList.addAll(secondaryPersonRepository.findAll());
-
-            if (personList.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(personList, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<List<Person>> getAllPersons() {
+        return readOnlyPersonService.getAllPersons();
     }
 
     @GetMapping("/person/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable("id") Long id) {
-        Optional<Person> personData = primaryPersonRepository.findById(id);
-
-        if (personData.isPresent()) {
-            return new ResponseEntity<>(personData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return readOnlyPersonService.getPersonById(id);
     }
 
     @PostMapping("/person")
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
-        try {
-            Person _person = new Person(person.getId(),person.getName());
-
-            primaryPersonRepository.save(_person);
-            return new ResponseEntity<>(_person, HttpStatus.CREATED);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            return personService.createPerson(person);
     }
 
     @PutMapping("/person/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable("id") Long id, @RequestBody Person person) {
-        Optional<Person> personData = primaryPersonRepository.findById(id);
-
-        if (personData.isPresent()) {
-            Person _person = personData.get();
-            _person.setName(_person.getName());
-            return new ResponseEntity<>(primaryPersonRepository.save(_person), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return personService.updatePerson(id, person);
     }
 
     @DeleteMapping("/person/{id}")
     public ResponseEntity<HttpStatus> deletePerson(@PathVariable("id") Long id) {
-        try {
-            primaryPersonRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return personService.deletePerson(id);
     }
 
     @DeleteMapping("/persons")
     public ResponseEntity<HttpStatus> deleteAllPersons() {
-        try {
-            primaryPersonRepository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
+        return personService.deleteAllPersons();
     }
 
 
--- src/main/java/com/example/spring_data/repository/primary/PrimaryPersonRepository.java
+++ src/main/java/com/example/spring_data/repository/primary/PrimaryPersonRepository.java
@@ -2,6 +2,8 @@ package com.example.spring_data.repository.primary;
 
 import com.example.spring_data.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PrimaryPersonRepository extends JpaRepository<Person, Long> {
 }
